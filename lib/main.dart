bb2530b766920fe118fb5559a4dfe8b73e9429da
MrCosik <56444584+MrCosik@users.noreply.github.com>
2021-04-23 10:36:09 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -12,11 +13,70 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: SizedBox.expand(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('images/every.png'),
+
+              Material(
+                shape: CircleBorder(),
+                elevation: 50,
+                child: CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage: AssetImage('images/every.png'),
+                  ),
+              ),
+                Text(
+                  'Kuba',
+                  style: TextStyle(
+                      fontSize: 40.5,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'),
                 ),
+                Text(
+                  'Stupid student',
+                  style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontFamily: 'Sans',
+                    fontSize: 15.0,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    thickness: 2,
+                    color: Colors.white30,
+                  ),
+                ),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+48 111 111 111',
+                        style: TextStyle(color: Colors.teal.shade900),
+                      ),
+                    )),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.mail,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'jakubmaster@gmail.com',
+                        style: TextStyle(color: Colors.teal.shade900),
+                      ),
+                    ))
               ],
             ),
           ),
